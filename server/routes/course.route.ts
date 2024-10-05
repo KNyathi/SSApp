1d410b3fe6c7391bac61b4612adb1e0b1d77469b
KNyathi <khayelihlenyathi07@gmail.com>
2024-10-05 23:30:06 +0300

    creating course routes

---
+++ server/routes/course.route.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
+
+const courseRouter = express.Router();
+
+courseRouter.post(
+  "/create-course",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  uploadCourse
+);
+
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editCourse
+);
+
+courseRouter.get(
+  "/get-course/:id",
+  getSingleCourse
+); //get the summary of the course but do not share the course (everyone has access)
+
+
+courseRouter.get(
+  "/get-courses",
+  getAllCourses
+);
+
+courseRouter.get(
+  "/get-course-content/:id",
+  isAuthenticated,
+  getCourseByUser
+);
+
+export default courseRouter;
